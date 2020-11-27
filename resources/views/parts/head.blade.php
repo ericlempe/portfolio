c6b280590085f20eacabe828a3e8fb5fa06a6528
Eric Lempe <dev.ericlp@gmail.com>
2020-11-26 23:36:06 -0300

    teste aws

--- resources/views/parts/head.blade.php
+++ resources/views/parts/head.blade.php
@@ -9,19 +9,19 @@
   <meta content="" name="descriptison">
   <meta content="" name="keywords">
 
-  <!-- Favicons -->
-  <link href="{{ asset('assets/img/favicon.png') }}" rel="icon">
-  <link href="{{ asset('assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+{{--  <!-- Favicons -->--}}
+{{--  <link href="{{ asset('assets/img/favicon.png') }}" rel="icon">--}}
+{{--  <link href="{{ asset('assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">--}}
 
-  <!-- Vendor CSS Files -->
-  <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('assets/vendor/font-awesome/css/font-awesome.min.css')  }}" rel="stylesheet">
-  <link href="{{ asset('assets/vendor/ionicons/css/ionicons.min.css')  }}" rel="stylesheet">
-  <link href="{{ asset('assets/vendor/owl.carousel/assets/owl.carousel.min.css')  }}" rel="stylesheet">
-  <link href="{{ asset('assets/vendor/venobox/venobox.css') }}" rel="stylesheet">
+{{--  <!-- Vendor CSS Files -->--}}
+{{--  <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">--}}
+{{--  <link href="{{ asset('assets/vendor/font-awesome/css/font-awesome.min.css')  }}" rel="stylesheet">--}}
+{{--  <link href="{{ asset('assets/vendor/ionicons/css/ionicons.min.css')  }}" rel="stylesheet">--}}
+{{--  <link href="{{ asset('assets/vendor/owl.carousel/assets/owl.carousel.min.css')  }}" rel="stylesheet">--}}
+{{--  <link href="{{ asset('assets/vendor/venobox/venobox.css') }}" rel="stylesheet">--}}
 
-  <!-- Template Main CSS File -->
-  <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+{{--  <!-- Template Main CSS File -->--}}
+{{--  <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">--}}
 
   <!-- =======================================================
   * Template Name: DevFolio - v2.3.0
